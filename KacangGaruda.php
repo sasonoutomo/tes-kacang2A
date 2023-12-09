3ef01276e0e88a9c9300501574f2e82bdb20e5fb
sasonoutomo <sasono.utomo.ronoatmojo@gmail.com>
2023-12-09 08:19:03 +0700

    Menambahkan file Kacang Garuda

---
+++ KacangGaruda.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kacang Garuda</title>
+</head>
+<body>
+    <h1>Kacang Garuda </h1>
+    <p>Apa itu kacang Garuda?
+        Garudafood | Indonesia's Leading Food and Beverage Company
+        Kacang kulit Garuda, terbuat dari kacang tanah pilihan yang garing dan renyah 
+        serta isi yang padat dengan kandungan asam lemak tak jenuh (unsaturated fatty acids)
+        di dalamnya yang dapat menurunkan risiko penyakit jantung koroner.</p>
+    
+</body>
+</html>
